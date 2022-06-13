b99e28a70e30ebb87b697649c819a8b7c844c2ba
Jepoy Factoran <factoran20@gmail.com>
2022-06-13 21:57:37 +0800

    added update and delete api resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -43,7 +43,7 @@ class ProductController extends Controller
      */
     public function show($id)
     {
-        //
+        return Product::find($id);
     }
 
     /**
@@ -55,7 +55,9 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $product = Product::find($id);
+        $product->update($request->all());
+        return $product;
     }
 
     /**
@@ -66,6 +68,6 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        return Product::destroy($id);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,10 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::get('/products',[ProductController::class,'index']);
-Route::post('/products',[ProductController::class,'store']);
+// Route::get('/products',[ProductController::class,'index']);
+// Route::post('/products',[ProductController::class,'store']);
+
+Route::resource('products', ProductController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
