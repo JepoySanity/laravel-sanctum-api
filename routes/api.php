9832544912a16ffcdd7bb66745b2920cb6b5745e
Jepoy Factoran <factoran20@gmail.com>
2022-06-13 22:23:27 +0800

    group routes that are public and private using the route group

--- routes/api.php
+++ routes/api.php
@@ -14,15 +14,20 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-// Route::get('/products',[ProductController::class,'index']);
-// Route::post('/products',[ProductController::class,'store']);
 
-Route::resource('products', ProductController::class);
+// Route::resource('products', ProductController::class);
+
+
+//public routes
+Route::get('/products',[ProductController::class,'index']);
+Route::get('/products/{id}',[ProductController::class,'show']);
 Route::get('products/search/{name}', [ProductController::class,'search']);
 
 //protected route group
 Route::group(['middleware'=>'auth:sanctum'],function(){
-
+    Route::post('/products',[ProductController::class,'store']);
+    Route::put('/products/{id}',[ProductController::class,'update']);
+    Route::delete('/products/{id}',[ProductController::class,'destroy']);
 });
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
